bc2eca9f2729892c3b3393dafe8b9afb84b5804f
diodev2 <19352823+iamDio@users.noreply.github.com>
2025-08-08 00:29:16 -0400

    initial commmit: PolicyBuilder quote api with nest.js, graphql, and redis

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+// src/app.module.ts
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { QuoteModule } from './quote/quote.module';
+import { RedisService } from './redis/redis.service';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    // Load environment variables
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // GraphQL setup
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+      introspection: true,
+    }),
+
+    QuoteModule,
+  ],
+  providers: [RedisService],
+  exports: [RedisService],
+})
+export class AppModule {}
